4393d13cc2ba43a99adb68c2f0cd19176edf7444
Michael Hughes <michaelmjh@icloud.com>
2021-01-23 17:05:48 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace PrimeNumbersOneToOneHundred
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersPrinted = 0;
+
+            for (int numberToCheck = 2; numberToCheck < 1000; numberToCheck++)
+            {
+                bool isPrime = true;
+
+                for (int y = 2; y < numberToCheck; y++)
+                {
+                    if ((numberToCheck % y) == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+                if (isPrime && numbersPrinted < 100)
+                {
+                    numbersPrinted++;
+                    Console.WriteLine($"{numbersPrinted}. {numberToCheck}");
+                    
+                }
+            }
+        }
+    }
+}
